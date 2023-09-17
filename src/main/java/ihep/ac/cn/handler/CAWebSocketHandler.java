00333609639285189c932f9ebb9909f452e78345
HuangLi <lihuang@ihep.ac.cn>
2023-09-17 17:58:49 +0800

    opt websocket

--- src/main/java/ihep/ac/cn/handler/CAWebSocketHandler.java
+++ src/main/java/ihep/ac/cn/handler/CAWebSocketHandler.java
@@ -1,32 +1,38 @@
 package ihep.ac.cn.handler;
 
 import ihep.ac.cn.entity.ResultEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CAWebSocketHandler implements WebSocketHandler {
 
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("WS: " + session.getId() + " connected");
         sendMessage(session, "success!");
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        sendMessage(session, ResultEntity.error("TEST", new double[]{1.0, 2.0, 3.0}).json());
+        log.info("WS: " + session.getId() + " received a message ==> " + message.getPayload());
+        sendMessage(session, ResultEntity.success("TEST", new double[]{1.0, 2.0, 3.0}).json());
     }
 
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        log.info("WS: " + session.getId() + " occurred a error ==> " + exception.getMessage());
         sendMessage(session, ResultEntity.error("Error", "Occurred error").json());
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        log.info("WS: " + session.getId() + " connection was closed; reason ==>" + closeStatus.getReason() + " cod ==> " + closeStatus.getCode());
         session.close();
     }
 
@@ -36,6 +42,7 @@ public class CAWebSocketHandler implements WebSocketHandler {
     }
 
     public void sendMessage(WebSocketSession session, String message) throws IOException {
+        log.info("WS: sent a message to ==> " + session.getId());
         TextMessage textMessage = new TextMessage(message);
         session.sendMessage(textMessage);
     }
